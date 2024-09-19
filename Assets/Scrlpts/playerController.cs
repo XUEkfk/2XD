c6eda290c19d725b8ec9e4378811f9e1dd816454
XUEkfk <love.kyngh.ky@gmail.com>
2024-09-19 16:30:31 +0800

    寫了鏡頭移動的腳本(跟隨滑鼠移動)

--- Assets/Scrlpts/playerController.cs
+++ Assets/Scrlpts/playerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public Transform viewPoint;
+    public float mousSensitivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"))*mousSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x,
+            transform.rotation.eulerAngles.z);
+
+        verticalRotStore += mouseInput.y;
+        verticalRotStore = Mathf.Clamp(verticalRotStore, -60f, 60f);
+
+        viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y,
+            viewPoint.rotation.eulerAngles.z);
+
     }
+    
+    
 }
