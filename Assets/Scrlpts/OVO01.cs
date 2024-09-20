84171d68ec6ce4222abc7f01530af24633299ef4
XUE_KFK <love.kyngh.ky@gmail.com>
2024-09-20 13:47:08 +0800

    遊戲開始囉

--- Assets/Scrlpts/OVO01.cs
+++ Assets/Scrlpts/OVO01.cs
@@ -7,7 +7,7 @@ public class OVO01 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("遊戲開始羅~");
     }
 
     // Update is called once per frame
