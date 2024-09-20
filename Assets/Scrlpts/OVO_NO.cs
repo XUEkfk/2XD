6d9d1f39fc1e18555cd06c085b1d7411ffad8c75
XUEKFK <love.kyngh.ky@gmail.com>
2024-09-20 13:53:06 +0800

    想寫時間增加的代碼

--- Assets/Scrlpts/OVO_NO.cs
+++ Assets/Scrlpts/OVO_NO.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class OVO_NO : MonoBehaviour
 {
+    public Time time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time ++;
     }
 }
