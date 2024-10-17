1daa321de0beb361392cee7bf80c3ce587192175
xuekfk <love.kyngh.ky@gmail.com>
2024-10-17 20:19:34 +0800

    跳躍依舊有問題

--- Assets/Scrlpts/playerController.cs
+++ Assets/Scrlpts/playerController.cs
@@ -1,179 +1,193 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public Transform viewPoint;
-    public float mousSensitivity = 1f;
-    public float moveSpeed = 3f,runSpeed = 10f,jumpForce = 12f,gr = 2.5f;
-    public Transform groundPonit;
-    public LayerMask groundLa;
-    public GameObject bulletImpact;
-    public float bllTime = 0.1f;
-    
-    private float verticalRotStore;
-    private Vector2 mouseInput;
-    private bool isGrounded;
-    private float shootCD;
+    [Header("玩家設定")]
+    public Transform viewPoint;           // 攝影機視角位置
+    public float mousSensitivity = 1f;    // 滑鼠靈敏度
+    public float moveSpeed = 3f;          // 移動速度
+    public float runSpeed = 10f;          // 跑步速度
+    public float jumpForce = 12f;         // 跳躍力量
+    public float gravityMultiplier = 2.5f;// 重力加成
+    public Transform groundPoint;         // 地面檢測點
+    public LayerMask groundLayer;         // 地面圖層
+    public GameObject bulletImpact;       // 子彈擊中效果
+    public float bulletFireRate = 0.1f;   // 子彈射擊頻率
 
-    public float MaxHt = 10f,HP=1f,CD=4f,OHC=5f;
-    private float hC;
-    private bool OH;
-   
-    //移動正規畫
-    private Vector3 moveDir,movement;
-    
-    //腳色控制器
-    public CharacterController Char;
-    
-    //設置CAMERA鏡頭
-    private Camera Cam;
-    
-    //設置一個加速的開關
-    private float Run;
-    
+    [Header("過熱設定")]
+    public float maxHeat = 10f;           // 最大過熱值
+    public float heatPerShot = 1f;        // 每次射擊增加的過熱值
+    public float coolDownRate = 4f;       // 冷卻速率
+    public float overHeatCooldown = 5f;   // 過熱後的冷卻時間
+
+    // 內部變量
+    private float verticalRotationStore;  // 垂直旋轉的暫存值
+    private Vector2 mouseInput;           // 滑鼠輸入
+    private bool isGrounded;              // 是否在地面
+    private float shootCooldown;          // 射擊冷卻時間
+    private float currentHeat;            // 當前過熱值
+    private bool isOverheated;            // 是否過熱
+
+    // 移動相關
+    private Vector3 moveDirection, movement;
+
+    // 組件引用
+    public CharacterController charController;  // 角色控制器
+    private Camera cam;                         // 攝影機
+    private float runMultiplier;                // 加速倍數
 
     // Start is called before the first frame update
     void Start()
     {
-        //滑鼠隱藏
+        // 鎖定滑鼠並隱藏
         Cursor.lockState = CursorLockMode.Locked;
-        //攝影機
-        Cam = Camera.main;
 
+        // 獲取攝影機
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"))*mousSensitivity;
-     
+        // 處理滑鼠輸入
+        HandleMouseInput();
+
+        // 處理移動
+        HandleMovement();
+
+        // 處理射擊
+        HandleShooting();
+    }
+
+    private void HandleMouseInput()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mousSensitivity;
+
+        // 水平旋轉
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-             
-             //鎖定滑鼠上下視移動最小值最大值
-        verticalRotStore += mouseInput.y;
-        verticalRotStore = Mathf.Clamp(verticalRotStore, -30f, 30f);
-     
-        viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y,viewPoint.rotation.eulerAngles.z);
 
-        //腳色移動
-        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        // 垂直旋轉，限制範圍
+        verticalRotationStore += mouseInput.y;
+        verticalRotationStore = Mathf.Clamp(verticalRotationStore, -30f, 30f);
+        viewPoint.rotation = Quaternion.Euler(-verticalRotationStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
+    }
 
-        //加速開關
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Run = runSpeed;
-        }
-        else
-        {
-            Run = moveSpeed;
-        }
+    private void HandleMovement()
+    {
+        // 讀取移動輸入
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
-        //紀錄Y軸值
-        float yVie = movement.y;
-        
-        //腳色移動
-        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized*Run;
-        movement.y = yVie;
-        
-        if (Input.GetButtonDown("Jump") /*&& isGrounded*/)
+        // 設置跑步加速
+        runMultiplier = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        // 記錄Y軸速度
+        float yVelocity = movement.y;
+
+        // 根據角色朝向移動
+        movement = (transform.forward * moveDirection.z + transform.right * moveDirection.x).normalized * runMultiplier;
+        movement.y = yVelocity;
+
+        // 檢測跳躍
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Debug.Log("jump");
+            Debug.Log("JUMP");
             movement.y = jumpForce;
         }
-        
-        isGrounded = Physics.Raycast(groundPonit.position, Vector3.down, 1f,groundLa);
-        
-        //若碰到地板則停止運算
-        if (Char.isGrounded)
+
+        // 檢測是否接觸地面
+        isGrounded = Physics.Raycast(groundPoint.position, Vector3.down, 1f, groundLayer);
+
+        // 如果角色在地面，將Y軸速度設為0
+        if (charController.isGrounded)
         {
             movement.y = 0f;
         }
-        //運算其值
-        movement.y += Physics.gravity.y*Time.deltaTime*gr;
-        
-        Char.Move(movement * moveSpeed * Time.deltaTime);
+
+        // 應用重力
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMultiplier;
+
+        // 移動角色
+        charController.Move(movement * moveSpeed * Time.deltaTime);
+
+        // 切換滑鼠鎖定狀態
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
         }
-        else if (Cursor.lockState == CursorLockMode.None)
+        else if (Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                
-            }
-            
+            Cursor.lockState = CursorLockMode.Locked;
         }
+    }
 
-        if (!OH)
+    private void HandleShooting()
+    {
+        if (isOverheated) // 如果過熱
         {
-           if ((Input.GetMouseButtonDown(0)))
-           {
-                      Shoot();
-           }
-          
-           if (Input.GetMouseButton(0))
-           {
-                      shootCD -= Time.deltaTime;
-                      
-                      if (shootCD<=0f)
-                      {
-                          Shoot();                
-                      }
-                      
-           }
-          
-           hC -= CD * Time.deltaTime;
-        }else
-        {
-            hC -= OHC * Time.deltaTime;
-            if (hC<=0)
+            currentHeat -= overHeatCooldown * Time.deltaTime;
+            if (currentHeat <= 0)
             {
-                OH = false;
+                isOverheated = false;
             }
         }
-
-        if (hC<0)
+        else
         {
-            hC = 0f;
+            if (Input.GetMouseButtonDown(0)) // 開始射擊
+            {
+                Shoot();
+            }
 
+            if (Input.GetMouseButton(0)) // 持續射擊
+            {
+                shootCooldown -= Time.deltaTime;
+                if (shootCooldown <= 0f)
+                {
+                    Shoot();
+                }
+            }
+
+            // 冷卻過程
+            currentHeat -= coolDownRate * Time.deltaTime;
         }
-        
+
+        // 過熱值不能低於0
+        if (currentHeat < 0)
+        {
+            currentHeat = 0f;
+        }
     }
 
     private void LateUpdate()
     {
-        //設置Camera即使沒有群組也會跟隨腳色
-        Cam.transform.position = viewPoint.position;
-        Cam.transform.rotation = viewPoint.rotation;
+        // 攝影機跟隨角色
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
     }
 
     private void Shoot()
     {
-        Ray ray = Cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        ray.origin = Cam.transform.position;
+        // 槍口射線
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        ray.origin = cam.transform.position;
 
-        if (Physics.Raycast(ray,out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Debug.Log("We hit" + hit.collider.gameObject.name);
+            Debug.Log("Hit: " + hit.collider.gameObject.name);
 
-           GameObject blletImpeobject= Instantiate(bulletImpact, hit.point+(hit.normal*0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
-        Destroy(blletImpeobject,3f);
-           
+            // 生成擊中效果
+            GameObject impact = Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
+            Destroy(impact, 3f);
         }
 
-        shootCD = bllTime;
+        shootCooldown = bulletFireRate; // 重置射擊冷卻時間
 
-        hC += HP;
-        if (HP>=MaxHt)
+        // 增加過熱值
+        currentHeat += heatPerShot;
+        if (currentHeat >= maxHeat) // 如果達到過熱狀態
         {
-            hC = MaxHt;
-            OH = true;
+            currentHeat = maxHeat;
+            isOverheated = true;
         }
     }
 }
